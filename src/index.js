5f97453c1746d08a21c9756fa57f110ff889d595
Tzu-Chi Lin <tzuchilin.0420@gmail.com>
2017-10-02 20:59:02 -0700

    Hello World

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+
+	return (
+		<div>
+			<h1>Hello  World</h1>
+			<p1>gooooooooood</p1>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 registerServiceWorker();
